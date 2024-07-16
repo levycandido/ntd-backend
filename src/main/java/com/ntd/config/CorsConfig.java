e7fe26ee00783b87f2cc82f28cdd429f2ecdee0b
levycandido <31323642+levycandido@users.noreply.github.com>
2024-07-16 13:22:29 -0300

    Changing S3 Path

--- src/main/java/com/ntd/config/CorsConfig.java
+++ src/main/java/com/ntd/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://challenge-ntd.s3-website.us-east-2.amazonaws.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
